5297731ca9f522f0cd505d72b6a9e769ca2e8243
Teja <teja.simma@artofliving.org>
2026-08-11 00:24:32 +0530

    Update baseURL and imageBaseURL to use the production server

--- app/mock_station/lib/config/app_config.dart
+++ app/mock_station/lib/config/app_config.dart
@@ -2,8 +2,8 @@ class AppConfig {
   // ============================================
   // Backend API
   // ============================================
-  static const String baseURL = 'http://172.16.69.1:6900/';
-  static const String imageBaseURL = 'http://172.16.69.1:6900/assets/userImages/';
+  static const String baseURL = 'https://app.mockstation.com/';
+  static const String imageBaseURL = 'https://app.mockstation.com/assets/userImages/';
 
   // ============================================
   // Firebase
